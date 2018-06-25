c8b770a61d465e9dce7df3a1ee67f7857e52e956
Dletta <jcduschletta@gmail.com>
2018-06-24 19:11:03 -0700

    latest test, to write 1 mil entries

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
+process.env.GUN_ENV = false
 var Gun = require('gun')
+
 var gun = Gun({
-  radisk: false,
-  until: 1
+  localStorage: false
 })
+
 
 var now = new Date()
 now = now.getMilliseconds()
 
 var app = gun.get('app').put({start: now})
 
-var writeRand = function() {
+var writeRand = function(timer, hr) {
   app.get(Gun.text.random()).put({name:"name"})
+  t++
+  l++
+  if (l == 1000) {
+    l = 0
+    console.log(process.uptime());
+    console.log(t);
+    var now = process.uptime()
+    var diff = now - hr
+    console.log(`at ${diff}`);
+    hr = now
+  }
+  if(t>=1000000){
+    clearInterval(timer)
+  }
 }
 
-let i = 10
+var t = 0
+var l = 0
+var hr = process.uptime()
 
-while(i>=0){
-  writeRand()
-  i--
-}
+console.log(`starting writes ${process.hrtime()}`);
+var timer = setInterval(writeRand, 1, timer, hr)
+console.log(`ending writes ${process.hrtime()}`);
